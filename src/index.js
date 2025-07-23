5d40703221a8d5e82f962d38f50962d8bd4631ef
Sarah Ma <sarahmma24@gmail.com>
2025-07-22 22:12:35 -0700

    Added some styling

--- src/index.js
+++ src/index.js
@@ -14,19 +14,29 @@ homeBtn.addEventListener('click', (e) => {
     e.preventDefault();
     contentDiv.innerHTML = "";
     loadHome();
+    setActiveButton(homeBtn);
 });
 
 menuBtn.addEventListener('click', (e) => {
     e.preventDefault();
     contentDiv.innerHTML = "";
     loadMenu();
+    setActiveButton(menuBtn);
 });
 
 aboutBtn.addEventListener('click', (e) => {
     e.preventDefault();
     contentDiv.innerHTML = "";
     loadAbout();
+    setActiveButton(aboutBtn);
 });
 
+function setActiveButton(activeBtn) {
+    [homeBtn, menuBtn, aboutBtn].forEach(btn => {
+        btn.style.color = '';
+    });
+    activeBtn.style.color = 'rgba(151, 151, 84, 1)';
+}
 
 loadHome();
+setActiveButton(homeBtn);
